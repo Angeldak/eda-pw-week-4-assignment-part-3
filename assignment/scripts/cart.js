1453b08e2751b22db100abcc10b6f768ec9d6194
Lloyd Prock <lloyd.prock@gmail.com>
2022-08-10 09:05:14 -0500

    inital setup for structure

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -1,3 +1,26 @@
 console.log('***** Cart Functions *****');
 // Make sure to test all functions here in the JS file!
 // We want to see how you are testing your code!!!
+
+let basket = [];
+const maxItems = 5;
+
+function addItem(item) {
+    
+} //End addItem
+
+function listItems() {
+
+} //End listItems
+
+function empty() {
+
+} // End empty
+
+function ifFull() {
+
+} //End ifFull
+
+function removeItem() {
+
+} //End removeItem
